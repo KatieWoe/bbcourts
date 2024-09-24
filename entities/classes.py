5d6578b66f26d592c10f48b2311122329218ec97
KatieWoe <kawo7071@colorado.edu>
2024-09-24 21:59:26 +0000

    Started folder and classes file to track entities

---
+++ entities/classes.py
@@ -0,0 +1,10 @@
+# This file holds the classes describing the entities for the bbcourts website
+
+# The Court class holds the features the a post about a court should have
+class Court:
+    #name of the court
+    name_of_court = ""
+    #array of all the ratings given to the court
+    rating_array = []
+    #average of the rating array
+    average_rating = Nan
