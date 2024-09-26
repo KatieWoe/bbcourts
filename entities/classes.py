b3acba2ed968eb21a52e2ac69b5c073c939a22a4
KatieWoe <kawo7071@colorado.edu>
2024-09-26 16:46:30 +0000

    Write Court setter functions

--- entities/classes.py
+++ entities/classes.py
@@ -4,27 +4,43 @@
 class Court:
     #name of the court
     name_of_court = ""
+    def set_name(self, name):
+        self.name_of_court = name
     
     #array of all the ratings given to the court
     rating_array = []
+    def add_rating(self, rating):
+        self.rating_array.append(rating)
     
     #average of the rating array
     average_rating = float("nan")
     
     #description of the court quality
     quality = ""
+    def set__quality(self, quality):
+        self.quality = quality
     
     #is the court accessible?
     ada_accessible = False
+    def set_ada(self, ada):
+        self.ada_accessible = ada
     
     #cost to get in
     admit_price = 0
+    def set_price(self, price):
+        self.admit__price = price
     
     #indoor or outdoor
     in_or_out = ""
+    def set_in_out(self, inout):
+        self.in_or_out = inout
     
     #location
     location = ""
+    def set_location(self, locate):
+        self.location = locate
     
     #Picture of court
     pic_of_court = ""
+    def set_pic(self, pic):
+        self.pic_of_court = pic
