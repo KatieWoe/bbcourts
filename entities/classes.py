f9d8de25b630b8dd45c7057f0218373b9e5cf2f0
KatieWoe <kawo7071@colorado.edu>
2024-09-26 16:02:40 +0000

    Fixed NaN usage in Court class

--- entities/classes.py
+++ entities/classes.py
@@ -4,7 +4,27 @@
 class Court:
     #name of the court
     name_of_court = ""
+    
     #array of all the ratings given to the court
     rating_array = []
+    
     #average of the rating array
-    average_rating = Nan
+    average_rating = float("nan")
+    
+    #description of the court quality
+    quality = ""
+    
+    #is the court accessible?
+    ada_accessible = False
+    
+    #cost to get in
+    admit_price = 0
+    
+    #indoor or outdoor
+    in_or_out = ""
+    
+    #location
+    location = ""
+    
+    #Picture of court
+    pic_of_court = ""
