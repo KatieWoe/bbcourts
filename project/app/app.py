23b37985c6907132ef913c834a8eb10255dee821
Jaekyeong Lee <jaekung@gmail.com>
2024-11-24 21:37:22 +0100

    Updated app.py

--- project/app/app.py
+++ project/app/app.py
@@ -1,4 +1,12 @@
-from courtfinder import app
+from flask import Flask
+
+app = Flask(__name__)
+
+from core.routes import core
+app.register_blueprint(core)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
