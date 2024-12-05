0cc138c9461b1b38ec74e5df929475f024f45b5a
KatieWoe <kawo7071@colorado.edu>
2024-12-05 05:01:55 +0000

    rest of photos

--- project/app/load_db.py
+++ project/app/load_db.py
@@ -16,6 +16,7 @@ def initialize_db():
     acc.createCourt("Dream Yard Court", 4, 1, 1, 1, 1, 0, '9-5', '$6', 'Fancy, Place', 'Rather overblown. Very popular, but overpriced and only open during buisnes hours. Has a fun pick up game scene, but only in the summer and mostly rich kids frequent it.')
     acc.createCourt("Play Place", 5, 1, 1, 1, 1, 1, '9-5', 'Free', 'Fancy, Place', 'A kid only court that holds lessons for beginers. Free for kids under 10 and their parents. A lovely, low stress place for kids to play.')
     print('Database loaded successfully')
+    
     acc.addPhoto(1, "https://robbreport.com/wp-content/uploads/2022/03/200803_EJ_waterline_square-044944_HIGH_RES.jpg?w=1000")
     acc.addPhoto(1, "https://assets-global.website-files.com/6173bb4ce523620d28c5c2d3/6174a0fd5b9a2b5471172a89_Hoop-City-Full-Court.jpg")
     acc.addPhoto(1, "https://nevco.com/wp-content/uploads/2021/02/750-Basketball-600x0-c-default.jpg")
@@ -30,14 +31,41 @@ def initialize_db():
     acc.addPhoto(3, "https://cdn.sqhk.co/2020newsportcourt/2020/3/geijeid/IMG_0240.JPG")
     acc.addPhoto(3, "https://st.hzcdn.com/simgs/pictures/home-gyms/indoor-home-basketball-court-groom-construction-co-inc-img~43e16aa409025659_14-1782-1-a57cb85.jpg")
     acc.addPhoto(3, "https://i.pinimg.com/236x/48/9b/5e/489b5ec4a8e89bdf35247ece508203d2.jpg")
+    
     acc.addPhoto(4, "https://www.sportcourtnortherncalifornia.com/wp-content/uploads/slider-basket-ball-court-putting-green.jpg")
     acc.addPhoto(4, "https://sportcourt.ca/wp-content/uploads/2019/04/mertesky-toronto-28x40-1024x765.jpg")
     acc.addPhoto(4, "https://sportcourt.ca/wp-content/uploads/2019/04/mandel-g-north-york-1024x768.jpg")
     acc.addPhoto(4, "https://img.posterstore.com/zoom/ps52266.jpg?auto=compress%2Cformat&fit=max&w=3840")
+    
     acc.addPhoto(5, "https://www.ymcalouisville.org/sites/default/files/2021-12/ymca-louisville-northeast-branch-5.jpg")
     acc.addPhoto(5, "https://www.ymcawf.org/images/images/DSC00018_med.jpg")
     acc.addPhoto(5, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4ZAVWWAI3GZ2PeT8ffQgEjVXr9VspX3Qqyg&s")
     acc.addPhoto(5, "https://ymcaboston.org/wp-content/uploads/2021/07/gymnasium-2.jpg")
+    
+    acc.addPhoto(6, "https://donaldearlcollins.com/wp-content/uploads/2010/09/0730101359a1.jpg")
+    acc.addPhoto(6, "https://www.courtsoftheworld.com/upload/courts/6586/2/COTW-drovers-way-1676202008.webp")
+    acc.addPhoto(6, "https://www.courtsoftheworld.com/upload/courts/12612/2/COTW-quriosa-1636887144.webp")
+    acc.addPhoto(6, "https://npr.brightspotcdn.com/dims4/default/df91789/2147483647/strip/true/crop/1000x687+0+0/resize/880x605!/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Flegacy%2Fsites%2Fdefault%2Ffiles%2Fpublic%2Fimages%2Farticle%2Fball-3.jpg")
+    
+    acc.addPhoto(7, "https://i.ytimg.com/vi/8q3xlGYxO5E/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCEtqJsNQpLxarUPNbOVUCeTbwIvQ")
+    acc.addPhoto(7, "https://static.happyly.com/media/Lafayette_Waneka_Lake_Park_1.jpg.1920x1080_q85_crop.jpghttps://static.happyly.com/media/Lafayette_Waneka_Lake_Park_1.jpg.1920x1080_q85_crop.jpg")
+    acc.addPhoto(7, "https://www.lafayetteco.gov/ImageRepository/Document?documentID=34941")
+    acc.addPhoto(7, "https://slidesandsunshine.com/wp-content/uploads/2020/09/Lafayette-Waneka-Lake-Park-12.jpg")
+    
+    acc.addPhoto(8, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSirZOuCCXnbPPPLIUogUsnELrsMsLzW2KyJw&s")
+    acc.addPhoto(8, "https://www.shutterstock.com/image-photo/basketball-hoop-camp-thorsmork-iceland-260nw-749466325.jpg")
+    acc.addPhoto(8, "https://www.shutterstock.com/image-photo/basketball-hoop-middle-nowhere-260nw-7248895.jpg")
+    acc.addPhoto(8, "https://media.gettyimages.com/id/1019146720/photo/morning-sun-rising-shine-onto-a-basketball-court-in-the-middle-of-gobi-desert-against-the.jpg?s=612x612&w=gi&k=20&c=wGcEOex6I_v4SJdXpF5rHnwIHYidJDyTwq9R9nY1mwY=")
+    
+    acc.addPhoto(9, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhR0zoM21zp7WqVGDDW0l6rG-O6t5mSPlXAw&s")
+    acc.addPhoto(9, "https://www.courtsoftheworld.com/blog/wp-content/uploads/2020/07/BLACK-MAMBA-House-1024x544.jpg")
+    acc.addPhoto(9, "https://www.courtsoftheworld.com/blog/wp-content/uploads/2020/05/HOuse-of-Mamba.jpg")
+    acc.addPhoto(9, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyfcGT2Rroz_p1hHRjBstphg2YCrHJe6MGIA&s")
+    
+    acc.addPhoto(10, "https://i.pinimg.com/originals/a9/0d/8d/a90d8def1549503617cbb3c6e9ffb905.jpg")
+    acc.addPhoto(10, "https://newhopeforkids.org/wp-content/uploads/2024/05/Basketball-court2023-e1679948036954.jpg")
+    acc.addPhoto(10, "https://assets.incstores.com/productimages/912x600/11120.jpg")
+    acc.addPhoto(10, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQroUHC922HoX1AjOx_Qv3xFMUeK32ruquG8fpavA7EknuskqUQGBpddFsAvTFkmvnhRX0&usqp=CAU")
     
 
 if __name__ == '__main__':
