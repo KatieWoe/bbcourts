237805fe81385e16b0d3a570f1dd6763c9e8a6b7
inch6134 <148062152+inch6134@users.noreply.github.com>
2024-12-04 19:49:48 -0700

    Added delete and create tables to load_db.py

--- project/app/load_db.py
+++ project/app/load_db.py
@@ -3,6 +3,8 @@ import access_methods as acc
 
 def initialize_db():
     """Initialise the database tables and insert initial data."""
+    db.delete_tables()
+    db.create_tables()
     acc.createCourt("Lakeshore Park Basketball Court", 4.5, 1, 1, 1, 1, 0, '10-18', '$5.99', 'Streeterville, Chicago', 'Nice indoor court')
     acc.createCourt("Douglas Park Outdoor Courts", 4, 0, 1, 1, 1, 1, '9-20', '$3.99', 'North Lawndale, Chicago', 'Most popular among locals')
     acc.createCourt("Margaret Hie Ding Lin Park Basketball Court", 5, 1, 0, 1, 0, 1, '11-19', '$4.00', 'Chinatown, Chicago', 'In a busy corner of Chinatown')
