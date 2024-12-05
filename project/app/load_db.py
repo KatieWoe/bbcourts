22aa94e9e261a247274d0fbd928f7a6d9f797fd0
inch6134 <148062152+inch6134@users.noreply.github.com>
2024-12-04 21:34:08 -0700

    Changes to court details

--- project/app/load_db.py
+++ project/app/load_db.py
@@ -10,9 +10,22 @@ def initialize_db():
     acc.createCourt("Margaret Hie Ding Lin Park Basketball Court", 5, 1, 0, 1, 0, 1, '11-19', '$4.00', 'Chinatown, Chicago', 'In a busy corner of Chinatown')
     acc.createCourt("Gill Park Basketball Court", 3, 1, 0, 1, 0, 1, '10-19', '$7.00', 'Lakeview East, Chicago', 'Expensive')
     print('Database loaded successfully')
+    # TODO: Change links
+    acc.addPhoto(1, "https://robbreport.com/wp-content/uploads/2022/03/200803_EJ_waterline_square-044944_HIGH_RES.jpg?w=1000")
+    acc.addPhoto(1, "https://robbreport.com/wp-content/uploads/2022/03/200803_EJ_waterline_square-044944_HIGH_RES.jpg?w=1000")
+    acc.addPhoto(1, "https://robbreport.com/wp-content/uploads/2022/03/200803_EJ_waterline_square-044944_HIGH_RES.jpg?w=1000")
     acc.addPhoto(1, "https://robbreport.com/wp-content/uploads/2022/03/200803_EJ_waterline_square-044944_HIGH_RES.jpg?w=1000")
     acc.addPhoto(2, "https://scissortailpark.org/wp-content/uploads/2022/10/306274736_807101607282596_7359110059786684243_n-1024x768.jpg")
+    acc.addPhoto(2, "https://scissortailpark.org/wp-content/uploads/2022/10/306274736_807101607282596_7359110059786684243_n-1024x768.jpg")
+    acc.addPhoto(2, "https://scissortailpark.org/wp-content/uploads/2022/10/306274736_807101607282596_7359110059786684243_n-1024x768.jpg")
+    acc.addPhoto(2, "https://scissortailpark.org/wp-content/uploads/2022/10/306274736_807101607282596_7359110059786684243_n-1024x768.jpg")
     acc.addPhoto(3, "https://www.deckhouse.com/wp-content/uploads/2022/10/Acorn-Deck-House-prefab-custom-indoor-home-basketball-court-addition-7-scaled.jpg")
+    acc.addPhoto(3, "https://www.deckhouse.com/wp-content/uploads/2022/10/Acorn-Deck-House-prefab-custom-indoor-home-basketball-court-addition-7-scaled.jpg")
+    acc.addPhoto(3, "https://www.deckhouse.com/wp-content/uploads/2022/10/Acorn-Deck-House-prefab-custom-indoor-home-basketball-court-addition-7-scaled.jpg")
+    acc.addPhoto(3, "https://www.deckhouse.com/wp-content/uploads/2022/10/Acorn-Deck-House-prefab-custom-indoor-home-basketball-court-addition-7-scaled.jpg")
+    acc.addPhoto(4, "https://www.sportcourtnortherncalifornia.com/wp-content/uploads/slider-basket-ball-court-putting-green.jpg")
+    acc.addPhoto(4, "https://www.sportcourtnortherncalifornia.com/wp-content/uploads/slider-basket-ball-court-putting-green.jpg")
+    acc.addPhoto(4, "https://www.sportcourtnortherncalifornia.com/wp-content/uploads/slider-basket-ball-court-putting-green.jpg")
     acc.addPhoto(4, "https://www.sportcourtnortherncalifornia.com/wp-content/uploads/slider-basket-ball-court-putting-green.jpg")
 
 if __name__ == '__main__':
